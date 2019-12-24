cacae9e60106b487a2767777eb1e8c996eb97982
Phill Barron <phillipbarron242@gmail.com>
2019-12-24 20:58:09 +0000

    now with more grover

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import grover from './grover.webp'
 import AlertButton from './AlertButton';
 import './App.css';
 
@@ -7,7 +7,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={grover} alt="logo" />
         <AlertButton />
       </header>
     </div>
